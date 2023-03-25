3be5c64838ea38460d064247f590e4b01a0ee774
GeroiGorodskoyZastroiki <denhardx@gmail.com>
2023-03-25 18:19:06 +0300

    player controller + .gitignore

---
+++ Assets/Resources/Player/PlayerController.cs
@@ -0,0 +1,269 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player")]
+    [Tooltip("Move speed of the character in m/s")]
+    public float MoveSpeed = 4.0f;
+    [Tooltip("Sprint speed of the character in m/s")]
+    public float SprintSpeed = 6.0f;
+    [Tooltip("Rotation speed of the character")]
+    public float RotationSpeed = 1.0f;
+    [Tooltip("Acceleration and deceleration")]
+    public float AccelerationFactor = 10.0f;
+
+    [Space(10)]
+    [Tooltip("The height the player can jump")]
+    public float JumpHeight = 1.2f;
+    [Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
+    public float Gravity = -15.0f;
+
+    [Space(10)]
+    [Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
+    public float JumpTimeout = 0.1f;
+    [Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
+    public float FallTimeout = 0.15f;
+
+    [Header("Player Grounded")]
+    [Tooltip("Useful for rough ground")]
+    public float GroundedOffset = -0.1f;
+    [Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
+    public float GroundedRadius = 0.5f;
+    [Tooltip("What layers the character uses as ground. PLAYER MUST BE AT THE DIFFERENT LAYER")]
+    public LayerMask GroundLayers;
+
+    [Header("Camera")]
+    [Tooltip("How far in degrees can you move the camera up")]
+    public float TopClamp = 90.0f;
+    [Tooltip("How far in degrees can you move the camera down")]
+    public float BottomClamp = -90.0f;
+
+    // cinemachine
+    private float cameraPitch;
+
+    // player
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float rotationVelocity;
+    [SerializeField]
+    private float verticalVelocity;
+    private float terminalVelocity = 53.0f;
+
+    // timeout deltatime
+    private float jumpTimeoutDelta;
+    private float fallTimeoutDelta;
+
+    private CharacterController controller;
+    private GameObject mainCamera;
+
+    private const float cameraRotationThreshold = 0.01f;
+
+    [Header("Character Input Values")]
+    [SerializeField]
+    private Vector2 move;
+    [SerializeField]
+    private Vector2 look;
+    [SerializeField]
+    private bool grounded = true;
+    [SerializeField]
+    private bool jump;
+    [SerializeField]
+    private bool sprint;
+
+    void Awake()
+    {
+        // get a reference to our main camera
+        if (mainCamera == null)
+        {
+            mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        }
+    }
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        // reset our timeouts on start
+        jumpTimeoutDelta = JumpTimeout;
+        fallTimeoutDelta = FallTimeout;
+    }
+
+    void Update()
+    {
+        JumpAndGravity();
+        GroundedCheck();
+        Move();
+        Debug.Log(move);
+    }
+
+    void LateUpdate()
+    {
+        CameraRotation();
+    }
+
+    private void GroundedCheck()
+    {
+        // set sphere position, with offset
+        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z);
+        // PLAYER MUST BE AT THE DIFFERENT LAYER
+        grounded = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers, QueryTriggerInteraction.Ignore);
+    }
+
+    private void CameraRotation()
+    {
+        // if there is an input
+        if (look.sqrMagnitude >= cameraRotationThreshold)
+        {
+            rotationVelocity = look.x * RotationSpeed;
+            cameraPitch += look.y * RotationSpeed;
+
+            // clamp our pitch rotation
+            cameraPitch = Mathf.Clamp(cameraPitch, BottomClamp, TopClamp);
+
+            // Update camera pitch
+            mainCamera.transform.localRotation = Quaternion.Euler(cameraPitch, 0.0f, 0.0f);
+
+            // rotate the player left and right
+            transform.Rotate(Vector3.up * rotationVelocity);
+        }
+    }
+
+    private void Move()
+    {
+        // set target speed based on move speed, sprint speed and if sprint is pressed
+        float targetSpeed = sprint ? SprintSpeed : MoveSpeed;
+
+        // a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
+
+        // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+        // if there is no input, set the target speed to 0
+        if (move == Vector2.zero) targetSpeed = 0.0f;
+
+        // a reference to the players current horizontal velocity
+        float currentHorizontalSpeed = new Vector3(controller.velocity.x, 0.0f, controller.velocity.z).magnitude;
+
+        float speedOffset = 0.1f;
+        float inputMagnitude = 1f;
+
+        // accelerate or decelerate to target speed
+        if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
+        {
+            // creates curved result rather than a linear one giving a more organic speed change
+            // note T in Lerp is clamped, so we don't need to clamp our speed
+            speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * AccelerationFactor);
+
+            // round speed to 3 decimal places
+            speed = Mathf.Round(speed * 1000f) / 1000f;
+        }
+        else
+        {
+            speed = targetSpeed;
+        }
+
+        // normalise input direction
+        Vector3 inputDirection = new Vector3(move.x, 0.0f, move.y).normalized;
+
+        // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+        // if there is a move input rotate player when the player is moving
+        if (move != Vector2.zero)
+        {
+            // move
+            inputDirection = transform.right * move.x + transform.forward * move.y;
+        }
+
+        // move the player
+        controller.Move(inputDirection.normalized * (speed * Time.deltaTime) + new Vector3(0.0f, verticalVelocity, 0.0f) * Time.deltaTime);
+    }
+
+    private void JumpAndGravity()
+    {
+        if (grounded)
+        {
+            // reset the fall timeout timer
+            fallTimeoutDelta = FallTimeout;
+
+            // stop our velocity dropping infinitely when grounded
+            if (verticalVelocity < 0.0f)
+            {
+                verticalVelocity = -2f;
+            }
+
+            // Jump
+            if (jump && jumpTimeoutDelta <= 0.0f)
+            {
+                // the square root of H * -2 * G = how much velocity needed to reach desired height
+                verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+            }
+
+            // jump timeout
+            if (jumpTimeoutDelta >= 0.0f)
+            {
+                jumpTimeoutDelta -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            // reset the jump timeout timer
+            jumpTimeoutDelta = JumpTimeout;
+
+            // fall timeout
+            if (fallTimeoutDelta >= 0.0f)
+            {
+                fallTimeoutDelta -= Time.deltaTime;
+            }
+
+            // if we are not grounded, do not jump
+            jump = false;
+        }
+
+        // apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
+        if (verticalVelocity < terminalVelocity)
+        {
+            verticalVelocity += Gravity * Time.deltaTime;
+        }
+    }
+
+    #region Inputs
+    public void OnMove(InputValue value)
+    {
+        move = value.Get<Vector2>();
+    }
+
+    public void OnLook(InputValue value)
+    {
+        look = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        jump = value.isPressed;
+    }
+
+    public void OnSprint(InputValue value)
+    {
+        sprint = value.isPressed;
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        Cursor.lockState = hasFocus ? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = !hasFocus;
+    }
+    #endregion
+
+    #region Editor
+    private void OnDrawGizmosSelected()
+    {
+        Color transparentGreen = new Color(0.0f, 1.0f, 0.0f, 0.35f);
+        Color transparentRed = new Color(1.0f, 0.0f, 0.0f, 0.35f);
+
+        if (grounded) Gizmos.color = transparentGreen;
+        else Gizmos.color = transparentRed;
+
+        // when selected, draw a gizmo in the position of, and matching radius of, the grounded collider
+        Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z), GroundedRadius);
+    }
+    #endregion Editor
+}
